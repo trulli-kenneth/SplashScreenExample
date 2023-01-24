f4490d822fb61ce407fe08d6b1a88c9654e6f596
trulli-kenneth <123408292+trulli-kenneth@users.noreply.github.com>
2023-01-24 12:30:41 -0500

    Initial Commit

---
+++ SplashScreenExample/SplashScreenExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreenExampleApp.swift
+//  SplashScreenExample
+//
+//  Created by Kenneth Adams on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreenExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
